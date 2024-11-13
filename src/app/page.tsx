9af679e71abc17e07dfaebc9eed30125322c6640
Julien-FRA <julien.francois@hetic.net>
2024-11-13 12:00:59 +0100

    <feat/client>: ajout couleur props + card components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,23 @@
-import Title from "@/components/atoms/Title";
-import Text from "@/components/atoms/Text";
-import SubTitle from "@/components/atoms/Subtitle";
-import Caption from "@/components/atoms/Caption";
-import Buttons from "@/components/atoms/Buttons";
-import BigTitle from "@/components/atoms/BigTitle";
-import LinkArrow from "@/components/atoms/LinkArrow";
-import Divider from "@/components/atoms/Divider";
+import Card from "@/components/molecules/Card";
 
 const Landing = () => {
   return (
     <>
-      <Title>Landing page</Title>
-      <SubTitle>Subtitle content</SubTitle>
-      <Text>Text landing page</Text>
-      <Caption>Text caption</Caption>
-      <Buttons colors={"blue"} filling={true} link={"/"}>
-        Envoyer
-      </Buttons>
-      <br />
-      <br />
-      <Buttons colors={"blue"} filling={false} link={"/"}>
-        Envoyer
-      </Buttons>
-      <br />
-      <br />
-      <BigTitle>Front-end developer</BigTitle>
-      <br />
-      <br />
-      <LinkArrow link={"/test"} colors={"white"}>
-        Learn more
-      </LinkArrow>
-      <br />
-      <br />
-      <Divider colors={"blue"} />
+      <Card
+        subTitle={"Goodmorning, Night City!"}
+        text={
+          "Yesterday’s body count lottery rounded out to a solid n’ sturdy thirty! "
+        }
+        cardLink={{
+          link: "/test",
+          text: "Learn more",
+        }}
+        colors={"white"}
+        textColors={"white"}
+        fill={false}
+        size={{ width: "30", height: "400" }}
+        corner={"topRight"}
+      />
     </>
   );
 };
--- src/components/atoms/Caption/index.tsx
+++ src/components/atoms/Caption/index.tsx
@@ -1,8 +1,25 @@
-import { ChildrenProps } from "@/types/Props";
 import styles from "./Caption.module.scss";
+import { ReactNode } from "react";
 
-const Caption = ({ children }: ChildrenProps) => {
-  return <h3 className={styles.caption}>{children}</h3>;
+type CaptionProps = {
+  colors: "yellow" | "blue" | "red" | "black" | "white";
+  children: ReactNode;
+};
+
+const Caption = ({ colors, children }: CaptionProps) => {
+  const colorsTheme = {
+    yellow: "#f8ef00",
+    blue: "#00F0FF",
+    red: "#FF003C",
+    black: "#000000",
+    white: "#fafafa",
+  };
+
+  return (
+    <h3 className={styles.caption} style={{ color: `${colorsTheme[colors]}` }}>
+      {children}
+    </h3>
+  );
 };
 
 export default Caption;
