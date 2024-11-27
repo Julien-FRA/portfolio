68307f4e131fdb8479f7729f60c9c52b2127ec55
Julien-FRA <julien.francois@hetic.net>
2024-11-28 00:10:50 +0100

    add typo and colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,26 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "../styles/typography.scss";
+
+export const degular_medium = localFont({
+  src: "../fonts/Degular-Medium.woff2",
+  variable: "--font-degular-medium",
+});
+
+export const degular_semiBold = localFont({
+  src: "../fonts/Degular-Semibold.woff2",
+  variable: "--font-degular-semiBold",
+});
+
+export const acumin_regular = localFont({
+  src: "../fonts/Acumin-RPro.woff2",
+  variable: "--font-acumin-regular",
+});
+
+export const acumin_bold = localFont({
+  src: "../fonts/Acumin-BdPro.woff2",
+  variable: "--font-acumin-bold",
+});
 
 export const metadata: Metadata = {
   title: "Julien FRANCOIS",
@@ -11,7 +33,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr">
+    <html
+      lang="fr"
+      className={`${degular_medium.variable} ${degular_semiBold.variable} ${acumin_regular.variable} ${acumin_bold.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,19 @@ export default function Home() {
   return (
     <>
       <header>Header</header>
-      <main>Main</main>
+      <main>
+        <h1>HeadingOne</h1>
+        <h2>HeadinngTwo</h2>
+        <h3>HeadingThree</h3>
+        <a href="" className="large">
+          Large link
+        </a>
+        <a href="" className="normal">
+          Normal link
+        </a>
+        <p className="text">Normal text</p>
+        <p className="text bold">Bold text</p>
+      </main>
       <footer>Footer</footer>
     </>
   );
