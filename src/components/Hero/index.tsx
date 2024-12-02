5dc6e07f74d7d9490fdb7e4f35ac52ef8a897e6a
Julien-FRA <julien.francois@hetic.net>
2024-12-02 17:35:07 +0100

    add hero section

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,33 @@
+import styles from "./Hero.module.scss";
+
+type HeroProps = {
+  location?: string;
+  title: string;
+  subtitle?: { normal: string; bold: string }[];
+};
+
+const Hero = ({ location, title, subtitle }: HeroProps) => {
+  return (
+    <div className={styles.hero}>
+      <div className={styles.wrapper}>
+        {location ? <p className={styles.heroLocation}>{location}</p> : null}
+        <h1 className={styles.heroTitle}>{title}</h1>
+        {subtitle
+          ? subtitle.map(
+              (item: { normal: string; bold: string }, key: number) => (
+                <div key={key} className={styles.heroSubtitle}>
+                  <p className={styles.heroSubtitleNormal}>{item.normal}</p>
+                  <span>→</span>
+                  <p className={`${styles.heroSubtitleBold} bold`}>
+                    {item.bold}
+                  </p>
+                </div>
+              )
+            )
+          : null}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
